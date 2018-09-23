e6c9030bd65b60ba80c7eade231f339f2017312c
Dusan Trtica <dtrtica@gogoair.com>
2018-09-23 14:57:07 +0200

    Added build

---
+++ webpack.config.production.js
@@ -0,0 +1,109 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WebpackChunkHash = require('webpack-chunk-hash');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { UnusedFilesWebpackPlugin } = require('unused-files-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const config = require('./webpack.config');
+
+const GLOBALS = {
+  'process.env': {
+    NODE_ENV: JSON.stringify('production'),
+  },
+  __DEV__: JSON.stringify(JSON.parse(process.env.DEBUG || 'false')),
+};
+
+module.exports = merge(config, {
+  mode: 'production',
+  output: {
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'jsfrontend-build'),
+    // publicPath: '%static_cdn_url%/',
+  },
+  entry: {
+    application: 'index',
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true,
+      }),
+    ],
+  },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.DefinePlugin(GLOBALS),
+    new webpack.HashedModuleIdsPlugin(),
+    new WebpackChunkHash(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.ejs'),
+      inject: false,
+      // base: '%static_cdn_url%',
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, 'src/assets'),
+        to: path.resolve(__dirname, 'jsfrontend-build'),
+      },
+    ]),
+    new UnusedFilesWebpackPlugin({
+      patterns: 'src/**/*.*',
+      globOptions: {
+        ignore: [
+          'node_modules/**/*',
+          'coverage/**/*',
+          '**/*.test.*',
+          '**/*.stories.js',
+          '**/*.data.js',
+          '**/assets/**/*',
+          '**/*.html',
+        ],
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              minimize: true,
+            },
+          },
+          { loader: 'postcss-loader' },
+          { loader: 'sass-loader', query: { outputStyle: 'compressed' } },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              minimize: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+});
